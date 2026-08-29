7a9ebce2f82190774792c31315d3492c6705e117
bubblerion <windowrains@naver.com>
2026-08-29 12:06:46 +0900

    함수 진입

--- ojs_flutter/lib/a2_3_2.dart
+++ ojs_flutter/lib/a2_3_2.dart
@@ -16,7 +16,9 @@ var b = true;
 var b2 = i < 10;
 var b3 = s.isEmpty;
 
-
+int f(int x){
+  return x + 10;
+}
 
 // 이것은 주석
 /**
@@ -27,14 +29,25 @@ void main(){
   // print('Hello, World!') 이건 에러
   // print('안녕하세요'); 이건 정상 동작, 세미콜론 여부
 
+  var numm = 0;
+  print(numm++);
+  print(++numm);
+  assert(2 == 2);
+  assert(2 != 3);
   assert(2+3 == 5);
   print(true && true);
   print(true || true);
   print(true == true);
   print('$_name은 $_age살입니다.');
   print('$_name은 ${_name.length} 글자입니다.');
-
-
-
-
+  int a = 10;
+  if (a is int){
+    print('정수');
+  }
+  // 실행 안됨
+  if (a is! int){
+    print('정수'); 
+  }
+  dynamic d = 30.5;
+  num number = d;
 }
