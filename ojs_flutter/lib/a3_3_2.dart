f297a63fbdecbaf04cca3e93df7f9dbb78a757a3
dailybutton <windowrains@dailybutton.co.kr>
2026-09-03 00:28:44 +0900

    새벽푸시

--- ojs_flutter/lib/a3_3_2.dart
+++ ojs_flutter/lib/a3_3_2.dart
@@ -1,2 +1,49 @@
 // 3.3.2 StatefulWidget 실습
 
+import 'package:flutter/material.dart';
+import 'package:ojs_flutter/main.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title : 'Flutter Demo',
+      theme : ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home : MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget{
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>{
+  var _text = 'Hello';
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title : Text('Hello World'),
+      ),
+      body : Text(
+        _text,
+        style : TextStyle(fontSize :  40),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          setState(() {
+            _text = 'World';
+          });
+        },
+        child : Icon(Icons.touch_app), 
+      ),
+    );
+  }
+}
