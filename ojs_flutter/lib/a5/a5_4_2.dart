2c24e7ac1f45b667710da92132bce5ee9ae914c9
dailybutton <windowrains@dailybutton.co.kr>
2026-09-14 08:54:07 +0900

    2609141

--- ojs_flutter/lib/a5/a5_4_2.dart
+++ ojs_flutter/lib/a5/a5_4_2.dart
@@ -3,3 +3,37 @@
 // AnimatedContainer 위젯은 한 화면 내에서 setState()함수를 호출하여 화면을 새로 그릴 때
 // 변경된 프로퍼티에 의해 애니메이션 되도록 해준다.
 
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class _AnimatedContainterPageState extends State<AnimatedContainerPage> {
+  var _size = 100.0;
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title : Text('AnimatedContainer'),
+      ),
+      body : Center(
+        child : GestureDetector(
+          onTap : (){
+            final random = Random(); // Random 클래스
+            setState(() {
+              // 클릭할 때마다 100.0~299.0 사이의 실수를 랜덤하게 얻기
+              _size = random.nextInt(200).toDouble() + 100;
+            });
+          },
+          child: AnimatedContainer(
+            duration: Duration(seconds: 1),
+            width: _size,
+            height: _size,
+            child: Image.asset('assets/sample.jpg'),
+            curve: Curves.fastOutSlowIn,
+          ),
+        )
+      )
+    );
+  }
+}
---
+++ ojs_flutter/lib/a5/a5_4_3.dart
@@ -0,0 +1,4 @@
+// 5.4.3 SilverAppBar와 SilverFillRemaining
+// SilverAppBar와 SilverFillRemaining은 화면 헤더를 동적으로 표현하는 위젯이다.
+// 헤더를 위로 스크롤하면 헤더 부분이 작아지면서 헤더 하단에 있던 정적인 내용만 보이는
+// AppBar 형태로 애니메이션 된다. 이런 효과를 Silver 효과라 부른다고 하겠다.
